2e902861b13f8dc3320af632eede6891d453309e
norbert99301 <49998769+norbert99301@users.noreply.github.com>
2021-11-15 06:57:21 -0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+
+<?php
+// $con = mysqli_connect("localhost","Emmanuel","password","harmoni");
+define("base_url", "http://localhost/midas/");
+$con = mysqli_connect("localhost","norbert99301","genesis99","midas");
+?>
+
