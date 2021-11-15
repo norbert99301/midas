6b8e2443d1fb01a95fa56581a8e6587a2b41de12
norbert99301 <49998769+norbert99301@users.noreply.github.com>
2021-11-15 06:56:18 -0800

    Add files via upload

---
+++ sell/index.php
@@ -0,0 +1,53 @@
+<?php
+include '../config.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BootstrapDash Comingsoon Template</title>
+    <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bd-coming-soon.css">
+</head>
+
+<body class="min-vh-100 d-flex flex-column">
+
+    <header>
+        <div class="container">
+            <nav class="navbar navbar-dark bg-transparenet">
+                <a class="navbar-brand" href="#">
+                    <!-- <img src="assets/images/midaslogo.svg" alt="logo" style="width:75px; height: 75px;"> -->
+                </a>
+              <!--   <span class="navbar-text ml-auto d-none d-sm-inline-block">978-130-5444 </span>
+                <span class="navbar-text d-none d-sm-inline-block">jerald_glover@glennie.tv</span> -->
+            </nav>
+        </div>
+    </header>
+    <main class="my-auto">
+        <div class="container">
+            <h1 style="text-align: center;" class="page-title">Coming soon...</h1>
+            <br> <br>
+            <div align="center"><a href="<?php echo base_url ?>">Back Home</a></div>
+           <!--  <p class="page-description">Lorem ipsum dolor sit amet consectetur adipisicing elit sed eiu sit amet consectetur
+            </p> -->
+           <!--  <p>Stay connected</p>
+            <nav class="footer-social-links">
+                <a href="#!" class="social-link"><i class="mdi mdi-facebook-box"></i></a>
+                <a href="#!" class="social-link"><i class="mdi mdi-twitter"></i></a>
+                <a href="#!" class="social-link"><i class="mdi mdi-google"></i></a>
+                <a href="#!" class="social-link"><i class="mdi mdi-slack"></i></a>
+                <a href="#!" class="social-link"><i class="mdi mdi-skype"></i></a>
+            </nav> -->
+        </div>
+    </main>
+</body>
+
+</html>
