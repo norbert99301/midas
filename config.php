8263de75e25e1c5c4224af61c04a618d072ff2e3
norbert99301 <49998769+norbert99301@users.noreply.github.com>
2021-11-22 19:17:12 -0800

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,24 @@
 <?php
 // $con = mysqli_connect("localhost","Emmanuel","password","harmoni");
 define("base_url", "https://midasdealings.herokuapp.com/");
-$con = mysqli_connect("localhost","norbert99301","genesis99","midas");
+
+// $con = mysqli_connect("localhost","norbert99301","genesis99","midas");
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+//connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_usernam, $cleardb_password, $cleardb_db);
+if ($conn) {
+echo "connected successfully";
+}
+else{
+	echo "connection not successful";
+}
 ?>
 
