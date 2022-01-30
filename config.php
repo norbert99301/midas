1ee1402249041fdb0fea881090e75521adad7539
norbert99301 <49998769+norbert99301@users.noreply.github.com>
2022-01-29 19:38:17 -0800

    Update config.php

--- config.php
+++ config.php
@@ -7,5 +7,5 @@ $con = mysqli_connect("localhost","norbert99301","genesis99","midas");
 
 
 
-define("base_url", "http://localhost/midas/");
+define("base_url", "https://midasdealings.herokuapp.com/");
 ?>
